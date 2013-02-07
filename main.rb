96114f6a9a9db28d6a206b6d2a0cf7edf92459d4
jesteracer <jesteracer@me.com>
2013-02-07 16:26:54 +0400

    Initial

---
+++ main.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+require 'haml'
+require 'sass'
+
+get '/' do
+  haml :index
+end
+
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  scss(:"stylesheets/#{params[:name]}")
+end
